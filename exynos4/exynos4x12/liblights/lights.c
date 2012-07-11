9c5d83d33a14da024064c03afe601ad855ceefca
XpLoDWilD <xplodgui@gmail.com>
2012-07-11 15:14:24 +0200

    lights: Use ALOG

--- exynos4/exynos4x12/liblights/lights.c
+++ exynos4/exynos4x12/liblights/lights.c
@@ -71,7 +71,7 @@ static int write_int(char const *path, int value)
 
     already_warned = 0;
 
-    LOGV("write_int: path %s, value %d", path, value);
+    ALOGV("write_int: path %s, value %d", path, value);
     fd = open(path, O_RDWR);
 
     if (fd >= 0) {
@@ -82,7 +82,7 @@ static int write_int(char const *path, int value)
         return amt == -1 ? -errno : 0;
     } else {
         if (already_warned == 0) {
-            LOGE("write_int failed to open %s\n", path);
+            ALOGE("write_int failed to open %s\n", path);
             already_warned = 1;
         }
         return -errno;
@@ -111,7 +111,7 @@ static int write_str(char const *path, const char* value)
 
     already_warned = 0;
 
-    LOGV("write_str: path %s, value %s", path, value);
+    ALOGV("write_str: path %s, value %s", path, value);
     fd = open(path, O_RDWR);
 
     if (fd >= 0) {
@@ -122,7 +122,7 @@ static int write_str(char const *path, const char* value)
         return amt == -1 ? -errno : 0;
     } else {
         if (already_warned == 0) {
-            LOGE("write_str failed to open %s\n", path);
+            ALOGE("write_str failed to open %s\n", path);
             already_warned = 1;
         }
         return -errno;
@@ -157,7 +157,7 @@ static int set_light_backlight(struct light_device_t *dev,
 
 static int close_lights(struct light_device_t *dev)
 {
-    LOGV("close_light is called");
+    ALOGV("close_light is called");
     if (dev)
         free(dev);
 
